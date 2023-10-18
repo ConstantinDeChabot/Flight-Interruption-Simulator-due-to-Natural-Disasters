316dfc49b472b26790f5586da3d9196065fc4dcc
Constantin de Chabot <trapwizard456@gmail.com>
2023-10-18 09:25:09 +0100

    Added API requests and SQL connection

---
+++ Flight Interrupt/API Response.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flight_Interrupt
+{
+
+    public class WeatherAPIResponse
+    {
+        public int queryCost { get; set; }
+        public float latitude { get; set; }
+        public float longitude { get; set; }
+        public string resolvedAddress { get; set; }
+        public string address { get; set; }
+        public string timezone { get; set; }
+        public float tzoffset { get; set; }
+        public Day[] days { get; set; }
+    }
+ 
+    public class Day
+    {
+        public string datetime { get; set; }
+        /*public int datetimeEpoch { get; set; }
+        public float tempmax { get; set; }
+        public float tempmin { get; set; }
+        public float temp { get; set; }
+        public float feelslikemax { get; set; }
+        public float feelslikemin { get; set; }
+        public float feelslike { get; set; }
+        public float dew { get; set; }
+        public float humidity { get; set; }
+        public float precip { get; set; }
+        public float precipprob { get; set; }
+        public float precipcover { get; set; }
+        public string[] preciptype { get; set; }
+        public float snow { get; set; }
+        public float snowdepth { get; set; }
+        public float windgust { get; set; }*/
+        public float windspeed { get; set; }
+        public float winddir { get; set; }
+        /*public float pressure { get; set; }
+        public float cloudcover { get; set; }
+        public float visibility { get; set; }
+        public float solarradiation { get; set; }
+        public float solarenergy { get; set; }
+        public float uvindex { get; set; }
+        public float severerisk { get; set; }
+        public string sunrise { get; set; }
+        public int sunriseEpoch { get; set; }
+        public string sunset { get; set; }
+        public int sunsetEpoch { get; set; }
+        public float moonphase { get; set; }
+        public string conditions { get; set; }
+        public string description { get; set; }
+        public string icon { get; set; }
+        public string[] stations { get; set; }
+        public string source { get; set; }*/
+    }
+
+}
--- Flight Interrupt/Program.cs
+++ Flight Interrupt/Program.cs
@@ -1,2 +1,70 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Flight_Interrupt;
+using System.Data.SqlServerCe;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+//READ API KEYS
+var path = @"\\strs/dfs/Devs/Data/17EDECHCo/! Github/Flight-Interruption-Simulator-due-to-Natural-Disasters/Flight Interrupt/Secrets.txt";
+string[] APIKeys = File.ReadAllLines(path);
+foreach (string line in APIKeys)
+{
+    Console.WriteLine(line);
+}
+
+
+//Flight Tracker API
+
+var distance = 250;
+var client = new HttpClient();
+var request = new HttpRequestMessage
+{
+    Method = HttpMethod.Get,
+    RequestUri = new Uri("https://adsbexchange-com1.p.rapidapi.com/v2/lat/51.46888/lon/-0.45536/dist/"+ distance +"/"),
+    Headers =
+    {
+        { "X-RapidAPI-Key", APIKeys[0] },
+        { "X-RapidAPI-Host", "adsbexchange-com1.p.rapidapi.com" },
+    },
+};
+using (var response = await client.SendAsync(request))
+{
+    response.EnsureSuccessStatusCode();
+    var body = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(body);
+}
+
+Console.WriteLine();
+
+
+//Weather Tracker API
+client = new HttpClient(); 
+request = new HttpRequestMessage
+{
+    Method = HttpMethod.Get,
+    RequestUri = new Uri("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london/today?unitGroup=metric&include=current&key="+APIKeys[1]+"&contentType=json"),
+    //https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/cheltenham/today?unitGroup=metric&include=current&key=64922XN86JX7F7TLDE3MXZ3B3&contentType=json
+};
+using (var response = await client.SendAsync(request))
+{
+    response.EnsureSuccessStatusCode();
+    var body = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(body);
+
+    var obj = JsonSerializer.Deserialize<WeatherAPIResponse>(body);
+
+    Console.WriteLine(obj.days[0].windspeed);
+    Console.WriteLine(obj.days[0].winddir);
+}
+
+
+//SQL
+string connectionString = @"Data Source=\\strs/dfs/evs/Data/17EDECHCo/! Github/Flight-Interruption-Simulator-due-to-Natural-Disasters/Flight Interrupt/VolcanoDatabase.sdf";
+using SqlCeConnection connection = new SqlCeConnection(connectionString);
+
+connection.Open();
+using (SqlCeCommand command = connection.CreateCommand())
+{
+    command.CommandText = "Select *";
+
+    var result = command.ExecuteScalar();
+}
