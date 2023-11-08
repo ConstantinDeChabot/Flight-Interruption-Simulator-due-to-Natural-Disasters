cec77efc7446ceeca8451db01f506edd165bfc94
Constantin de Chabot <trapwizard456@gmail.com>
2023-11-08 10:55:55 +0000

    Put project in class

--- Flight Interrupt/Program.cs
+++ Flight Interrupt/Program.cs
@@ -3,68 +3,94 @@ using System.Data.SqlServerCe;
 using System.Net.Http.Headers;
 using System.Text.Json;
 
-//READ API KEYS
-var path = @"\\strs/dfs/Devs/Data/17EDECHCo/! Github/Flight-Interruption-Simulator-due-to-Natural-Disasters/Flight Interrupt/Secrets.txt";
-string[] APIKeys = File.ReadAllLines(path);
-foreach (string line in APIKeys)
+namespace Flight_Interrupt
 {
-    Console.WriteLine(line);
-}
 
-
-//Flight Tracker API
-
-var distance = 250;
-var client = new HttpClient();
-var request = new HttpRequestMessage
-{
-    Method = HttpMethod.Get,
-    RequestUri = new Uri("https://adsbexchange-com1.p.rapidapi.com/v2/lat/51.46888/lon/-0.45536/dist/"+ distance +"/"),
-    Headers =
+    class Program
     {
-        { "X-RapidAPI-Key", APIKeys[0] },
-        { "X-RapidAPI-Host", "adsbexchange-com1.p.rapidapi.com" },
-    },
-};
-using (var response = await client.SendAsync(request))
-{
-    response.EnsureSuccessStatusCode();
-    var body = await response.Content.ReadAsStringAsync();
-    Console.WriteLine(body);
-}
+        //initialise APIs
+        public static HttpClient client = new HttpClient();
+        public static HttpRequestMessage request = new HttpRequestMessage();
 
-Console.WriteLine();
+        static async Task Main(string[] args)
+        {
+            //READ API KEYS
+            var path = @"\\strs/dfs/Devs/Data/17EDECHCo/! Github/Flight-Interruption-Simulator-due-to-Natural-Disasters/Flight Interrupt/Secrets.txt";
+            string[] APIKeys = File.ReadAllLines(path);
+            foreach (string line in APIKeys)
+            {
+                Console.WriteLine(line);
+            }
+
+            //Flight Tracker API
+
+            var distance = 250;
+            client = new HttpClient();
+            request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://adsbexchange-com1.p.rapidapi.com/v2/lat/51.46888/lon/-0.45536/dist/"+ distance +"/"),
+                Headers =
+                {
+                    { "X-RapidAPI-Key", APIKeys[0] },
+                    { "X-RapidAPI-Host", "adsbexchange-com1.p.rapidapi.com" },
+                },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(body);
+            }
+
+            Console.WriteLine();
 
 
-//Weather Tracker API
-client = new HttpClient(); 
-request = new HttpRequestMessage
-{
-    Method = HttpMethod.Get,
-    RequestUri = new Uri("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london/today?unitGroup=metric&include=current&key="+APIKeys[1]+"&contentType=json"),
-    //https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/cheltenham/today?unitGroup=metric&include=current&key=64922XN86JX7F7TLDE3MXZ3B3&contentType=json
-};
-using (var response = await client.SendAsync(request))
-{
-    response.EnsureSuccessStatusCode();
-    var body = await response.Content.ReadAsStringAsync();
-    Console.WriteLine(body);
+            //Weather Tracker API
+            client = new HttpClient(); 
+            request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + Longitude + "%2C%" + Latitude +"?unitGroup=metric&include=current&key="+APIKeys[1]+"&contentType=json"),
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(body);
 
-    var obj = JsonSerializer.Deserialize<WeatherAPIResponse>(body);
+                var obj = JsonSerializer.Deserialize<WeatherAPIResponse>(body);
 
-    Console.WriteLine(obj.days[0].windspeed);
-    Console.WriteLine(obj.days[0].winddir);
-}
+                Console.WriteLine(obj.days[0].windspeed);
+                Console.WriteLine(obj.days[0].winddir);
+            }
 
+            //SQL
+            //connect VS to SQL database
+            string connectionString = @"Data Source=\\strs/dfs/Devs/Data/17EDECHCo/! Github/Flight-Interruption-Simulator-due-to-Natural-Disasters/Flight Interrupt/VolcanoDatabase.sdf";
+            using SqlCeConnection connection = new SqlCeConnection(connectionString);
+            connection.Open();
 
-//SQL
-string connectionString = @"Data Source=\\strs/dfs/evs/Data/17EDECHCo/! Github/Flight-Interruption-Simulator-due-to-Natural-Disasters/Flight Interrupt/VolcanoDatabase.sdf";
-using SqlCeConnection connection = new SqlCeConnection(connectionString);
+            //create variables to use
+            SqlCeCommand command; //used to read into the database
+            SqlCeDataReader dataReader; //used to get data specified by query
+            string sql = ""; //contains the sql query
 
-connection.Open();
-using (SqlCeCommand command = connection.CreateCommand())
-{
-    command.CommandText = "Select *";
+            //give query, send command and receive data
+            sql = "select VolcanoName, Longitude, Latitude from VolcanoDatabase where country = 'Japan'"; //TEST QUERY (GIVE ALL VOLCANOS FROM JAPAN)
+            command = new SqlCeCommand(sql, connection);
+            dataReader = command.ExecuteReader();
 
-    var result = command.ExecuteScalar();
+            //ouput
+            while (dataReader.Read()) 
+            {
+                Console.WriteLine(dataReader.GetValue(0) + " - " + dataReader.GetValue(1) + ", " + dataReader.GetValue(2));
+            }
+
+            //close the objects
+            dataReader.Close();
+            command.Dispose();
+            connection.Close();
+        }
+    }
 }
